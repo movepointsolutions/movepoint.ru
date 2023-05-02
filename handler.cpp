42dcaa41423e1ed5a055bf60edde1f60556da539
Anek <anek@movepoint.ru>
2023-05-02 05:13:16 +0300

    serve static by nginx

--- handler.cpp
+++ handler.cpp
@@ -17,60 +17,6 @@ request_handler::request_handler(const std::string &root, request_type &&req)
 {
 }
 
-// Return a reasonable mime type based on the extension of a file.
-beast::string_view
-mime_type(beast::string_view path)
-{
-    using beast::iequals;
-    auto const ext = [&path]
-    {
-        auto const pos = path.rfind(".");
-        if(pos == beast::string_view::npos)
-            return beast::string_view{};
-        return path.substr(pos);
-    }();
-    if(iequals(ext, ".htm"))  return "text/html";
-    if(iequals(ext, ".html")) return "text/html";
-    if(iequals(ext, ".php"))  return "text/html";
-    if(iequals(ext, ".css"))  return "text/css";
-    if(iequals(ext, ".txt"))  return "text/plain";
-    if(iequals(ext, ".js"))   return "application/javascript";
-    if(iequals(ext, ".json")) return "application/json";
-    if(iequals(ext, ".xml"))  return "application/xml";
-    if(iequals(ext, ".swf"))  return "application/x-shockwave-flash";
-    if(iequals(ext, ".flv"))  return "video/x-flv";
-    if(iequals(ext, ".png"))  return "image/png";
-    if(iequals(ext, ".jpe"))  return "image/jpeg";
-    if(iequals(ext, ".jpeg")) return "image/jpeg";
-    if(iequals(ext, ".jpg"))  return "image/jpeg";
-    if(iequals(ext, ".gif"))  return "image/gif";
-    if(iequals(ext, ".bmp"))  return "image/bmp";
-    if(iequals(ext, ".ico"))  return "image/vnd.microsoft.icon";
-    if(iequals(ext, ".tiff")) return "image/tiff";
-    if(iequals(ext, ".tif"))  return "image/tiff";
-    if(iequals(ext, ".svg"))  return "image/svg+xml";
-    if(iequals(ext, ".svgz")) return "image/svg+xml";
-    if(iequals(ext, ".ogg"))  return "audio/ogg";
-    return "application/text";
-}
-
-// Append an HTTP rel-path to a local filesystem path.
-// The returned path is normalized for the platform.
-std::string
-path_cat(
-        beast::string_view base,
-        beast::string_view path)
-{
-    if(base.empty())
-        return std::string(path);
-    std::string result(base);
-    char constexpr path_separator = '/';
-    if(result.back() == path_separator)
-        result.resize(result.size() - 1);
-    result.append(path.data(), path.size());
-    return result;
-}
-
 message_generator request_handler::bad_request(beast::string_view why)
 {
     http::response<http::string_body> res{http::status::bad_request, request.version()};
@@ -109,7 +55,6 @@ message_generator request_handler::empty_body(beast::string_view target)
 {
     http::response<http::empty_body> res{http::status::ok, request.version()};
     res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-    res.set(http::field::content_type, mime_type(target));
     res.keep_alive(request.keep_alive());
     return res;
 }
@@ -240,48 +185,5 @@ message_generator request_handler::response()
         return empty_body(target);
     }
 
-    // Build the path to the requested file
-    std::string path = path_cat(doc_root, target);
-    if(request.target().back() == '/')
-        path.append("index.html");
-
-    // Body to be returned
-    http::file_body::value_type body;
-
-    // Attempt to open the file
-    beast::error_code ec;
-    body.open(path.c_str(), beast::file_mode::scan, ec);
-
-    // Handle the case where the file doesn't exist
-    if(ec == beast::errc::no_such_file_or_directory)
-        return not_found(request.target());
-
-    // Handle an unknown error
-    if(ec)
-        return server_error(ec.message());
-
-    // Cache the size since we need it after the move
-    auto const size = body.size();
-
-    // Respond to HEAD request
-    if(request.method() == http::verb::head)
-    {
-        http::response<http::empty_body> res{http::status::ok, request.version()};
-        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-        res.set(http::field::content_type, mime_type(path));
-        res.content_length(size);
-        res.keep_alive(request.keep_alive());
-        return res;
-    }
-
-    // Respond to GET request
-    http::response<http::file_body> res{
-        std::piecewise_construct,
-            std::make_tuple(std::move(body)),
-            std::make_tuple(http::status::ok, request.version())};
-    res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-    res.set(http::field::content_type, mime_type(path));
-    res.content_length(size);
-    res.keep_alive(request.keep_alive());
-    return res;
+    return not_found(request.target());
 }
